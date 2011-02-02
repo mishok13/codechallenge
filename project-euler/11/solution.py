8e77cadef9e0a8252d416405579a2656732411ca
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-02 13:42:03 +0200

    Done, this was pretty easy when done with bruteforcing.

--- project-euler/11/solution.py
+++ project-euler/11/solution.py
@@ -5,10 +5,11 @@
 import argparse
 import sys
 import traceback
-from operator import itemgetter
+from operator import mul
 
 
 def consecutives(table, width, height):
+    """Find all diagonals, rows and columns in the table and yield them"""
     for row in xrange(height):
         yield [table[(row, column)] for column in xrange(width)]
     for column in xrange(height):
@@ -20,7 +21,26 @@ def consecutives(table, width, height):
     for row in xrange(height):
         yield [table[(row - column, column)] for column in xrange(row + 1)]
     for column in xrange(width):
-        yield [table[(height - 1 - row, column + row)] for row in xrange(width - column)]
+        yield [table[(height - row - 1, column + row)]
+               for row in xrange(width - column)]
+
+
+def adjacents(consecutive, length):
+    for start in xrange(len(consecutive) + 1 - length):
+        yield consecutive[start:start+length]
+
+
+def product(seq):
+    return reduce(mul, seq, 1)
+
+
+def produce(table, length):
+    width, height = max(table.keys())
+    for consecutive in consecutives(table, width + 1, height + 1):
+        if len(consecutive) >= length:
+            for adjacent in adjacents(consecutive, length):
+                yield product(adjacent)
+
 
 def process(args):
     try:
@@ -28,9 +48,7 @@ def process(args):
         for row, line in enumerate(args.input):
             for column, number in enumerate(map(int, line.strip().split())):
                 table[(row, column)] = number
-        width, height = max(table.keys())
-        for consecutive in consecutives(table, width + 1, height + 1):
-            print consecutive
+        print max(produce(table, args.length))
     except Exception:
         traceback.print_exc()
         return 2
