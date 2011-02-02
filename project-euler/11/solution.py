def380aa31dae89c8e93be1fe2328ce3a47b74b8
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-02 13:19:22 +0200

    Reverse diagonals are hard for me :(

--- project-euler/11/solution.py
+++ project-euler/11/solution.py
@@ -5,11 +5,22 @@
 import argparse
 import sys
 import traceback
+from operator import itemgetter
 
 
-def print_consecutives(table):
-    raise NotImplementedError
-
+def print_consecutives(table, width, height):
+    for row in xrange(height):
+        print [table[(row, column)] for column in xrange(width)]
+    for column in xrange(height):
+        print [table[(row, column)] for row in xrange(height)]
+    for row in xrange(height):
+        print [table[(column + row, column)] for column in xrange(width - row)]
+    for column in xrange(width):
+        print [table[(row, row + column)] for row in xrange(height - column)]
+    for row in xrange(height):
+        print [(row - column, column) for column in xrange(row + 1)]
+    for column in xrange(width):
+        print [(height - 1 - row, column + row) for row in xrange(width - column)]
 
 def process(args):
     try:
@@ -17,7 +28,8 @@ def process(args):
         for row, line in enumerate(args.input):
             for column, number in enumerate(map(int, line.strip().split())):
                 table[(row, column)] = number
-        print_consecutives(table)
+        width, height = max(table.keys())
+        print_consecutives(table, width + 1, height + 1)
     except Exception:
         traceback.print_exc()
         return 2
