a65418862153239e7ce858f55a5d4018e8a08b7e
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-15 17:25:16 +0200

    This was too simple, methinks

--- project-euler/38/solution.py
+++ project-euler/38/solution.py
@@ -7,9 +7,24 @@ import sys
 import traceback
 
 
+def is_pandigital(number):
+    return len(number) == 9 and len(set(number)) == 9 and '0' not in number
+
+
+
+def produce(limit):
+    for number in xrange(limit):
+        for multiplier_limit in xrange(3, 8):
+            concat = ''.join(str(number * multiplier)
+                             for multiplier in xrange(1, multiplier_limit))
+            if is_pandigital(concat):
+                yield int(concat), number
+
+
+
 def process(args):
     try:
-        raise NotImplementedError
+        print max(produce(args.limit))
     except Exception:
         traceback.print_exc()
         return 2
@@ -24,5 +39,5 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-
+    parser.add_argument('-l', '--limit', default=100, type=int)
     main(parser.parse_args())
