c96a3a4f3b0fff0d5c8d437ebd64657dcc304880
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-05-27 18:49:03 +0300

    This is overly simple with shapely

--- project-euler/102/solution.py
+++ project-euler/102/solution.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python
 
 
-from math import sqrt
+
+import argparse
+from shapely.geometry import Polygon, Point
+import sys
 
 
-def semiperimeter(a, b, c):
-    return sum([a, b, c]) / 2.0
 
-
-def heron(a, b, c):
-    s = semiperimeter(a, b, c)
-    return sqrt(s * (s - a) * (s - b) * (s -c))
-
-
-def side(first, second):
-    return sqrt((first[0] - second[0])**2 + (first[1] - second[1])**2)
-
-
-def main():
-    points = [(-340,495), (-153,-910), (835,-947)]
-    outer = [side(points[0], points[1]), side(points[1], points[2]), side(points[2], points[0])]
-    print outer, heron(*outer)
-    sides = [side((0, 0), point) for point in points]
-    print heron(sides[0], sides[1], outer[0]) + heron(sides[1], sides[2], outer[1]) + heron(sides[2], sides[0], outer[2])
-
+def main(args):
+    origin = Point(0, 0)
+    print sum(Polygon(zip(*([iter(map(int, line.strip().split(',')))] * 2))).intersects(origin) for line in args.input)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', nargs='?', type=argparse.FileType('r'),
+                        default=sys.stdin)
+    main(parser.parse_args())
