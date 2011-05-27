4c5555fa1665f03cf888afeb9e6a308992b82b21
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-05-27 20:25:28 +0300

    Whatever

--- project-euler/31/solution.py
+++ project-euler/31/solution.py
@@ -7,15 +7,17 @@ import sys
 import traceback
 from itertools import product, chain, ifilter, imap
 from collections import defaultdict
+import cPickle as pickle
 
 
 available = [1, 2, 5, 10, 20, 50, 100, 200]
 
 
-def solve(value):
-    cache = {0: [], 1: set([(1, )]),
-             # 2: set([(1, 1), (2, )])
-             }
+def solve(value, cache=None):
+    if cache is None:
+        cache = {0: [], 1: set([(1, )]),
+                 2: set([(1, 1), (2, )])
+                 }
     def solver(val):
         try:
             return cache[val]
@@ -29,15 +31,24 @@ def solve(value):
                     solution.add((coin, ))
             cache[val] = solution
             return solution
-    return solver(value)
+    return solver(value), cache
 
 
 def main(args):
-    ret = solve(args.value)
+    try:
+        with open(args.cache) as cachefile:
+            cache = pickle.load(cachefile)
+    except IOError:
+        cache = None
+    ret, cache = solve(args.value, cache)
+    with open(args.cache, 'w') as cachefile:
+        pickle.dump(cache, cachefile)
     print len(ret)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--value', default=200, type=int)
+    parser.add_argument('-c', '--cache', default='cache.db')
     main(parser.parse_args())
