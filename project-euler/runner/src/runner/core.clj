5f6c4170e9aa922d62cc4877d4f3b1d23d9da623
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-08-02 21:37:14 +0300

    Returned to my old project

--- project-euler/runner/src/runner/core.clj
+++ project-euler/runner/src/runner/core.clj
@@ -1,1 +1,11 @@
 (ns runner.core)
+
+(download code)
+(defn main
+  [config]
+  (let [overall-limit (get config "limit.overall")
+        per-run-limit (get config "limit.perrun")
+        runs-limit (get config "limit.runs")]
+    (map
+     (partial run overall-limit per-run-limit runs-limit)
+     (collect-solutions (get config "solutions.ignore")))))
